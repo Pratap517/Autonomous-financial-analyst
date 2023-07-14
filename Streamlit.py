6ebe0d96624d8f5fc180213109e90a2d411ad490
Prathap Reddy <65093897+Pratap517@users.noreply.github.com>
2023-07-14 17:53:37 +0530

    Update Streamlit.py
    added api

--- Streamlit.py
+++ Streamlit.py
@@ -9,9 +9,11 @@ import json
 import streamlit as st
 
 
-load_dotenv(find_dotenv())
-SERPAPI_API_KEY = os.getenv("SERPAPI_API_KEY")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+# load_dotenv(find_dotenv())
+# SERPAPI_API_KEY = os.getenv("SERPAPI_API_KEY")
+# SERPAPI_API_KEY = os.getenv("SERPAPI_API_KEY")
+OPENAI_API_KEY = "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"
+SERPAPI_API_KEY = "50630e438aacdabfba9b63ad7b6d3bf6d86a0331"
 
 
 def search_financials(company_name):
