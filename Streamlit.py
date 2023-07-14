5ce00847c5e8d0819fe4e3e7d4d7e368bd84d4fe
Prathap Reddy <65093897+Pratap517@users.noreply.github.com>
2023-07-14 19:05:20 +0530

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -34,8 +34,8 @@ def search_financials(company_name):
     return response_data
 
 
-def summarise_financial_statements(response_str, company, balance_sheet_last_year):
-    response_str = json.dumps(response_str)
+def summarise_financial_statements(response_data, company, balance_sheet_last_year):
+    response_str = json.dumps(response_data)
     try:
         llm = OpenAI(model_name="gpt-3.5-turbo", temperature=0.7)
     except Exception as e:
